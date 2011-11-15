dab6344a1ced71fc4da95dd4149fe753315aeb77
Kenneth Reitz <me@kennethreitz.com>
2011-11-15 10:44:18 -0500

    Short.

--- spark.py
+++ spark.py
@@ -1,24 +1,25 @@
 # -*- coding: utf-8 -*-
 
+"""
+spark.py
+~~~~~~~~
+
+A port of @holman's spark project for Python.
+"""
+
 import sys
 
 ticks = (u'▁', u'▂', u'▃', u'▅', u'▆', u'▇')
 
+
 def spark_string(ints):
     """Returns a spark string from given iterable of ints."""
     step = ((max(ints) - min(ints)) / (len(ticks) - 1))
-
-    s = []
-    for i in ints:
-        s.append(ticks[(i/step)])
-        s.append(' ')
-
-    return u''.join(s)
+    return u''.join([ticks[(i/step)] for i in ints])
 
 
 def spark_print(ints, stream=None):
     """Prints spark to given stream."""
-
     if stream is None:
         stream = sys.stdout
 
