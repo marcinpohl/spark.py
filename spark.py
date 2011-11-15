46de3add00fcf91699340bf81aa56fc066a8933e
Kenneth Reitz <me@kennethreitz.com>
2011-11-15 10:44:58 -0500

    spaces

--- spark.py
+++ spark.py
@@ -15,7 +15,7 @@ ticks = (u'▁', u'▂', u'▃', u'▅', u'▆', u'▇')
 def spark_string(ints):
     """Returns a spark string from given iterable of ints."""
     step = ((max(ints) - min(ints)) / (len(ticks) - 1))
-    return u''.join([ticks[(i/step)] for i in ints])
+    return u' '.join([ticks[(i/step)] for i in ints])
 
 
 def spark_print(ints, stream=None):
